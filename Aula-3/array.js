9959e840dcbb2fe1b75d391a2c0761351203302b
JairOliveiraDev <jair.correa.oliveira@gmail.com>
2022-03-31 15:04:41 -0300

    learning

--- Aula-3/array.js
+++ Aula-3/array.js
@@ -1,1 +1,21 @@
 // array
+const animals = [
+    'lion',
+    'monkey',
+    'cat',
+    //add object in a array
+    //Obs não misturar tipos diferentes de dados 
+    {
+        especie: 'bird',
+        name: 'bibi'
+    }
+]
+
+//acessar valores dentro do array
+console.log(animals[2]);
+
+//saber tamanho do array
+console.log(animals.length);
+
+//acessar objeto dentro de uma array
+console.log(animals[3].name);// result 'bibi'
