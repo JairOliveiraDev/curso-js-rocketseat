0c17ab68cd0502788b47c82a253dfda1333d0fee
JairOliveiraDev <jair.correa.oliveira@gmail.com>
2022-04-01 00:08:29 -0300

    learning

---
+++ Aula-4/functions-4.js
@@ -0,0 +1,18 @@
+// function hoisting
+console.log(sayMyName());
+//funçoes declaras assim sofre hoisting
+// ou seja a função é elevada e pode ser chamada antes da funçao
+function sayMyName(nome) {
+    nome = 'jair'
+    return nome;
+}
+
+//console.log(sayYourName());
+//função anonima
+// não ocorre o hoisting 
+const sayYourName = function(nome) {
+    nome = 'maike'
+    return nome;
+}
+
+console.log(sayYourName());
