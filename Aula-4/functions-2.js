0858df13dcd5a838b242e373c838bb713e9e032e
JairOliveiraDev <jair.correa.oliveira@gmail.com>
2022-03-31 23:31:45 -0300

    learning

---
+++ Aula-4/functions-2.js
@@ -0,0 +1,15 @@
+// funçoes dentro de variaveis
+// função anonima
+// function
+// parametros da funçao(parameters)
+const sum = function (num1, num2) {
+    return num1 + num2
+}
+
+let num1 = 34;
+let num2 = 25;
+console.log(sum(10, 23.50).toFixed(2));//10, 23,50 são argumentos
+
+console.log(`o numero 1 é ${num1}`);
+console.log(`o numero 2 é ${num2}`);
+console.log(`resultado da soma é ${sum(num1, num2)}`);
