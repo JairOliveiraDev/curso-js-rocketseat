6de39f7f4ea024183201a20775602b2c1f7a0422
JairOliveiraDev <jair.correa.oliveira@gmail.com>
2022-04-01 19:38:35 -0300

    learning

--- Aula-5/prototype.js
+++ Aula-5/prototype.js
@@ -5,3 +5,16 @@
    * prototype chain
    * _proto_
 */
+
+//manipulando numeros
+
+let number = 345.33452345;
+//exibe numero com 2 casas decimais e troca o ponto por virgula
+console.log(number.toFixed(2).replace('.', ','));
+
+//text transfor
+word = 'programar é muito bom!!';
+console.log(word.toLowerCase());
+console.log(word.toUpperCase());
+
+//manipulando strings e arrays
