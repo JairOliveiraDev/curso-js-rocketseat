2559b922a4f2084bd00620aa100d6a88300ffe89
JairOliveiraDev <jair.correa.oliveira@gmail.com>
2022-04-04 11:18:40 -0300

    learn

--- Aula-6/Expressoes-e-operadores.js
+++ Aula-6/Expressoes-e-operadores.js
@@ -15,7 +15,7 @@ name.surName = "Brito"
 let age = new Number(23);
 console.log(name, age);
 
-//datas
+//dates
 let data = new Date('2022-04-04');
 //use __proto__ para ver os metodos desse objeto no console do navegador
 console.log(data.__proto__);
@@ -29,3 +29,43 @@ console.log(person.name, person.age);
 console.log(person);
 delete person.age
 console.log(person);
+
+// Operadores de atribuição (Assignment)
+let x
+
+// assignment normal:
+x = 1
+
+// addition assignment (adição):
+x += 2
+
+// subtraction assignment (subtração):
+x -= 1
+
+// multiplication assignment (multiplacação):
+x *= 2
+
+// division assignment (divisão):
+x /= 2
+
+// exponetiation assignment (exponenciação):
+x **= 2
+
+// remainder assignment (resto de divisão):
+x %= 2
+
+//operador ternario
+// condition ? valule1 : value2
+
+//exemplo café top
+let pao = true;
+let queijo = true;
+
+const niceBreackFast = pao && queijo ? 'café top' : 'café ruim';
+console.log(niceBreackFast);
+
+const nice = pao || queijo ? 'café top' : 'café ruim';
+console.log(nice);
+
+const niceBreack = !pao && !queijo ? 'café top' : 'café ruim';
+console.log(niceBreack);
