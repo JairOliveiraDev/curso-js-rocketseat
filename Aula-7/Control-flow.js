43af5248b93aa10a4969c9d38af59a301990f7c6
JairOliveiraDev <jair.correa.oliveira@gmail.com>
2022-04-04 22:18:59 -0300

    learn control flow

--- Aula-7/Control-flow.js
+++ Aula-7/Control-flow.js
@@ -1,11 +1,26 @@
 // control flow
 
 // if ... else
-let temperature = 38.5;
+let temperature = 37.4;
 
 if(temperature >= 37.5) {
     console.log(`sua temperatura é: ${temperature.toFixed(2)}, você esta com febre alta `);
 } else if(temperature < 37.5 && temperature >= 37) {
-    console.log(`sua temperatura é: ${temperature.toFixed(2)}, você não esta com febre  moderada`)
+    console.log(`sua temperatura é: ${temperature.toFixed(2)}, você esta com febre moderada`)
+} else {
+    console.log(`sua temperatura é: ${temperature.toFixed(2)}, você esta com febre `)
 }
 
+/*
+let temperature = 36.9
+let highTemperature = temperature >= 37.5
+let mediumTemperature = temperature < 37.5 && temperature >= 37
+
+if(highTemperature) {
+    console.log('Febre alta')
+} else if(mediumTemperature) {
+    console.log('Febre moderada')
+} else {
+    console.log('Saudável')
+}
+*/
