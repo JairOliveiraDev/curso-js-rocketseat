7228b89626301c1a45a7b9e13b628211a1c99758
JairOliveiraDev <jair.correa.oliveira@gmail.com>
2022-04-05 08:17:15 -0300

    loop

---
+++ Aula-8/loop-for.js
@@ -0,0 +1,10 @@
+//loop for of
+//for(inicialização de uma variável; condição de continuação para o loop; expressão final)
+
+for(let i = 100; i > 0; i--) {
+    console.log(i)
+}
+console.log('agora o laço segundo laço for');
+for(let i = 1; i <= 10; i++) {
+    console.log(i)
+}
