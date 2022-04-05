959897ca12974fbea26860b7482adef631415b4a
JairOliveiraDev <jair.correa.oliveira@gmail.com>
2022-04-04 22:31:48 -0300

    switch case

--- Aula-7/switch.js
+++ Aula-7/switch.js
@@ -1,1 +1,36 @@
 // switch
+/* let carOption = 'b';
+switch (carOption) {
+    case 'a':
+        console.log('voce escolheu a')
+        //codigo
+        break
+    case 'b':
+        console.log('voce escolheu b')
+        //codigo
+        break
+    default:
+        console.log('voce escolheu default')
+
+        break
+}
+*/
+
+function calculate(number1, operator, number2) {
+    let result
+
+    switch (operator) {
+        case '+':
+            break
+        case '-':
+            break
+        case '*':
+            break
+        case '/':
+            break
+        default:
+            console.log('não implementado');
+            break            
+
+    }
+}
