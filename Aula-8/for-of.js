0147328b511ec50a999c9bb231e81ad18683b83e
JairOliveiraDev <jair.correa.oliveira@gmail.com>
2022-04-12 09:30:18 -0300

    loop for of

---
+++ Aula-8/for-of.js
@@ -0,0 +1,14 @@
+// for of
+
+let nome = "Mayk";
+let names = ['Joao', 'Paulo', 'Pedro'];
+
+//pega e imprime uma letra do nome até terminar
+for(let char of nome) {
+    console.log(char);
+}
+
+//iprime todos os nomes do array
+for(let firstName of names) {
+    console.log(firstName);
+}
